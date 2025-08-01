bd035df2b79c8724c62b9bed3c8849a40a47e3fa
Rizki Cahya <rizki.cahya03@gmail.com>
2025-08-01 22:12:44 +0700

    feat: make logic connection for local or docker

--- connection.php
+++ connection.php
@@ -1,8 +1,10 @@
 <?php
 
-$hostname = "db";
-$username = "bloguser";
-$password = "blogpass";
+$isDocker = strpos($_SERVER['REMOTE_ADDR'], '172.') === 0 || getenv('DOCKER') === 'true';
+
+$hostname = $isDocker ? 'db' : 'localhost'; # db for docker, localhost for laragon/local
+$username = $isDocker ? "bloguser" : 'root';
+$password = $isDocker ? "blogpass" : '';
 $database = "db_blog";
 
 $connection = mysqli_connect($hostname, $username, $password, $database);
